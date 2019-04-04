5c8a672d84ab7b02f0a091b9247c5df9f852463a
yudetamago <yudetamago.orz@gmail.com>
2019-04-04 14:38:50 +0900

    Add CLI command

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from cbc_casper_simulator.examples.one_validator_all_states import simulate
+from cbc_casper_simulator.examples.broadcast import simulate as broadcast
+from cbc_casper_simulator.examples.lmd_ghost import simulate as lmd_ghost
+import argparse
 
-simulate()
+parser = argparse.ArgumentParser(description='CBC Casper Simulator')
+parser.add_argument(
+    '--example', help='broadcast or lmd_ghost', default='broadcast')
+args = parser.parse_args()
 
-
+if args.example == 'broadcast':
+    broadcast()
+elif args.example == 'lmd_ghost':
+    lmd_ghost()
+else:
+    print("Error: argument is invalid")
