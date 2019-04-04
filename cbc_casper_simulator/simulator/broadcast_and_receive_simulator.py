e89d334903fc3aa4aca20d4379e1bb90c33ad6f9
yudetamago <yudetamago.orz@gmail.com>
2019-04-04 18:06:01 +0900

    Refactor broadcast simulator

--- cbc_casper_simulator/simulator/broadcast_and_receive_simulator.py
+++ cbc_casper_simulator/simulator/broadcast_and_receive_simulator.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
-from typing import Iterable
-from cbc_casper_simulator.message import Message
+from typing import Iterator
 from cbc_casper_simulator.validator_set import ValidatorSet
 from cbc_casper_simulator.network.model import Model as NetworkModel
 from cbc_casper_simulator.util.ticker import Ticker
 from cbc_casper_simulator.simulator.simulator_config import SimulatorConfig
 
 
-class RandomCreationAndBroadcastSimulator(Iterable[NetworkModel]):
+class BroadCastAndReceiveSimulator(Iterator[NetworkModel]):
     def __init__(self,
                  config: SimulatorConfig
                  ):
@@ -22,30 +21,24 @@ class RandomCreationAndBroadcastSimulator(Iterable[NetworkModel]):
 
     def __next__(self) -> NetworkModel:
         i = self.ticker.current()
-        if i == 0:
-            current = self.network
-        elif i > self.config.max_slot:
+        if i > self.config.max_slot:
             raise StopIteration
-        else:
-            current = self.step()
+        if i % 2 == 0:
+            self.broadcast_from_random_validator()
+        self.all_validators_receive_all_packets()
         self.ticker.tick()
-        return current
+        return self.network
 
-    def step(self) -> NetworkModel:
-        validator_set = self.network.validator_set
-        if self.ticker.current() % 2 == 0:
-            # The validator randomly selected create and broadcast a message to other validators
-            sender = validator_set.choice_one()
-            message = sender.create_message()
-            res = sender.add_message(message)
-            assert res.is_ok(), res.value
-            self.network.broadcast(message, sender)
+    def broadcast_from_random_validator(self):
+        sender = self.network.validator_set.choice_one()
+        message = sender.create_message()
+        res = sender.add_message(message)
+        assert res.is_ok(), res.value
+        self.network.broadcast(message, sender)
 
-        # Validators receive queued messages in every slot
-        for receiver in validator_set.all():
+    def all_validators_receive_all_packets(self):
+        for receiver in self.network.validator_set.all():
             packets = self.network.receive(receiver)
             for packet in packets:
                 res = receiver.add_message(packet.message)
                 assert res.is_ok(), res.value
-
-        return self.network
