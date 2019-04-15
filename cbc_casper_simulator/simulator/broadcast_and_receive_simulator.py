bf17c09348d2a0a73290e9aaa78b0e868a0979e4
yudetamago <yudetamago.orz@gmail.com>
2019-04-15 16:03:49 +0900

    Add block proposer selection

--- cbc_casper_simulator/simulator/broadcast_and_receive_simulator.py
+++ cbc_casper_simulator/simulator/broadcast_and_receive_simulator.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
-from typing import Iterator
+from typing import Iterator, List, Optional
 from cbc_casper_simulator.validator_set import ValidatorSet
 from cbc_casper_simulator.validator import Validator
 from cbc_casper_simulator.network.model import Model as NetworkModel
 from cbc_casper_simulator.util.ticker import Ticker
+from cbc_casper_simulator.util.sha256 import SHA256
 from cbc_casper_simulator.simulator.config import Config
+import random as r
 
 
 class BroadCastAndReceiveSimulator(Iterator[NetworkModel]):
@@ -37,7 +39,8 @@ class BroadCastAndReceiveSimulator(Iterator[NetworkModel]):
         self.network.join(new_validator)
 
     def broadcast_from_random_validator(self):
-        sender = self.network.validator_set.choice_one()
+        sender = self.block_proposer()
+        assert sender is not None, "no block proposer"
         message = sender.create_message()
         if message.estimate.is_checkpoint(self.config.checkpoint_interval):
             self.validator_rotation(self.ticker.current())
@@ -52,3 +55,23 @@ class BroadCastAndReceiveSimulator(Iterator[NetworkModel]):
             for packet in packets:
                 res = receiver.add_message(packet.message)
                 assert res.is_ok(), "{} ({})".format(res.value, receiver.name)
+
+    def block_proposer(self) -> Optional[Validator]:
+        validators: List[Validator] = self.network.validator_set.validators
+        for validator in validators:
+            head = validator.create_estimate()
+            active_validators = head.active_validators
+            # Global random oracle for block proposer election
+            # We can get the same block proposer if slot and validators are same
+            r.seed(self.gen_hash(self.ticker.current(), active_validators))
+            proposer_index = r.randint(0, len(active_validators) - 1)
+            proposer = active_validators[proposer_index]
+            if proposer == validator:
+                return proposer
+        return None
+
+    @classmethod
+    def gen_hash(cls, slot: int, validators: List[Validator]) -> int:
+        sorted_validators = sorted(validators, key=lambda validator: validator.hash)
+        text = str(slot) + " " + ''.join([str(validator.hash) for validator in sorted_validators])
+        return int.from_bytes(SHA256.digest(text), byteorder='little')
